4e28084e0c80e494e62735f4a36cbb9fa190bacd
Vass0 <e.huidobro01@ufromail.cl>
2024-09-01 15:09:27 -0400

    inicializando  develop

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @BeforeAll
+    public static void iniciar(){
+      Main lista = new Main();
+    }
+    @BeforeEach
+    public void crearLista(){
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void verificarEdad() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verificarBoleto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validarInvitados() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void aforoDisponible() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ingresarPersona() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void permitirEntrada() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removerPersonal() {
+    }
+}
