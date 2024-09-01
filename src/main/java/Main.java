474355d478b849924b7b98e006affb62db896a9e
Vass0 <e.huidobro01@ufromail.cl>
2024-09-01 15:09:27 -0400

    inicializando  develop

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    String [][] listaEvento = new String[10][5];
+    int aforoTotalVip =
+    public static boolean verificarEdad(String[][] lista){
+
+    }
+
+    public static String verificarBoleto(String[][] lista){
+
+    }
+
+    public static boolean validarInvitados(String[][] lista){
+
+    }
+
+    public static int aforoDisponible(String[][] lista){
+
+    }
+
+    public static String[][] ingresarPersona(String[][] lista){
+
+    }
+
+    public static boolean permitirEntrada(String[][] lista){
+
+    }
+
+    public static String[][] removerPersonal(String[][] lista){
+
+    }
+
+}
