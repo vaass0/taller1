8fc90008298a79d7bb4cd3e02cd789f8bd848214
Vass0 <e.huidobro01@ufromail.cl>
2024-09-01 15:45:19 -0400

    Prueba verificar boleto completa

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,24 +8,52 @@ class MainTest {
     int aforoGenMax = 20;
     String[][] listaEvento = new String[10][5];
     Main gestion = new Main();
-
     @BeforeEach
-    public void llenarLista() {
-        listaEvento[0] = new String[]{"Pedro", "25", "General", "0", "True"};
-        listaEvento[1] = new String[]{"Eduardo", "20", "Vip", "1", "False"};
-        listaEvento[2] = new String[]{"Pepito", "15", "False", "0", "False"};
-        listaEvento[3] = new String[]{"Mark", "28", "Vip", "0", "False"};
-        listaEvento[4] = new String[]{"Jose", "40", "General", "0", "True"};
+    public void llenarLista(){
+        listaEvento[0] = new String[] {"Pedro","25","General","0","True"};
+        listaEvento[1] = new String[] {"Eduardo","20","Vip","1","False"};
+        listaEvento[2] = new String[] {"Pepito","15","False","0","False"};
+        listaEvento[3] = new String[] {"Mark","28","Vip","0","False"};
+        listaEvento[4] = new String[] {"Jose","40","General","0","True"};
+        System.out.println("Se han cargado los invitados...");
 
     }
 
     @org.junit.jupiter.api.Test
     void verificarEdad() {
-        assertTrue(gestion.verificarEdad(listaEvento, 0));
-        assertTrue(gestion.verificarEdad(listaEvento, 1));
-        assertFalse(gestion.verificarEdad(listaEvento, 2));
-        assertTrue(gestion.verificarEdad(listaEvento, 3));
-        assertTrue(gestion.verificarEdad(listaEvento, 4));
+        assertTrue(gestion.verificarEdad(listaEvento,0));
+        assertTrue(gestion.verificarEdad(listaEvento,1));
+        assertFalse(gestion.verificarEdad(listaEvento,2));
+        assertTrue(gestion.verificarEdad(listaEvento,3));
+        assertTrue(gestion.verificarEdad(listaEvento,4));
+    }
+
+    @org.junit.jupiter.api.Test
+    void verificarBoleto() {
+        assertEquals("General",gestion.verificarBoleto(listaEvento,0));
+        assertEquals("Vip",gestion.verificarBoleto(listaEvento,1));
+        assertEquals("False",gestion.verificarBoleto(listaEvento,2));
+        assertEquals("Vip",gestion.verificarBoleto(listaEvento,3));
+        assertEquals("General",gestion.verificarBoleto(listaEvento,4));
+    }
+
+    @org.junit.jupiter.api.Test
+    void validarInvitados() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void aforoDisponible() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ingresarPersona() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void permitirEntrada() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removerPersonal() {
     }
 }
-
